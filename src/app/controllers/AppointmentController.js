a845eedfbf660ed432bf1574a36eb63f8292d65d
Paulo Henrique <phmelomorais@gmail.com>
2019-07-18 22:25:42 -0300

    Using date-fns to validade appointment rules

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,6 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -15,16 +17,37 @@ class AppointmentController {
 
     const { provider_id, date } = req.body;
 
-    const isProvider = await User.findOne({
+    /** Check if provider_id is a provider */
+    const CheckIsProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     });
 
-    if (!isProvider) {
+    if (!CheckIsProvider) {
       return res
         .status(401)
         .json({ error: 'You can only create appointmentes with providers' });
     }
 
+    /** Check for past dates */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    /** Check date availability */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'You already have an appointment at this hour' });
+    }
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
