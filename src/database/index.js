e78d441da3f398ccb85d6cdf76e3adcdfdaaf0f2
Paulo Henrique <phmelomorais@gmail.com>
2019-07-19 20:10:14 -0300

    Saving Data MongoDB. Using date-fns/locale

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,6 @@
 import Sequelize from 'sequelize';
 import mongoose from 'mongoose';
+
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
